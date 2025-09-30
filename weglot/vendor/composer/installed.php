99fc333eba9e91b5d222020e43ee837cead6d959
Sean Murphy <sean@iamseanmurphy.com>
2025-09-30 07:29:36 +0000

    5.1

--- weglot/vendor/composer/installed.php
+++ weglot/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'weglot/translate-wordpress',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '05af3cdedb610b0e2674c2b9faf6b0405f966aa6',
+        'reference' => 'b9243ab4d04ff366d1ebfd73ed34c1c29781ecdc',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -29,8 +29,8 @@
             'dev_requirement' => false,
         ),
         'symfony/polyfill-php80' => array(
-            'pretty_version' => 'v1.32.0',
-            'version' => '1.32.0.0',
+            'pretty_version' => 'v1.33.0',
+            'version' => '1.33.0.0',
             'reference' => '0cc9dd0f17f61d8131e7df6b84bd344899fe2608',
             'type' => 'library',
             'install_path' => __DIR__ . '/../symfony/polyfill-php80',
@@ -49,7 +49,7 @@
         'weglot/translate-wordpress' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '05af3cdedb610b0e2674c2b9faf6b0405f966aa6',
+            'reference' => 'b9243ab4d04ff366d1ebfd73ed34c1c29781ecdc',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
